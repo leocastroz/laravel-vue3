9476238ceab8defa715290f6b3df5db7ce871c64
leocastroz <castroz.dev@gmail.com>
2024-10-15 16:39:26 -0300

    fix: remove route

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeComponent from './components/HomeComponent.vue';
-import Dashboard from '@//Pages/Dashboard.vue';
+import Dashboard from '@/Pages/Dashboard.vue';
 import Contrato from '@/Pages/Contrato.vue';
 import Profile from '@/Pages/Profile.vue';
 
 const routes = [
-    // { path: '/', component: HomeComponent },
     { path: '/dashboard', component: Dashboard },
     { path: '/contrato', component: Contrato },
     { path: '/profile', component: Profile },
