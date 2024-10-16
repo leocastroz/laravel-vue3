a83b811edde99d0080fd37c3248cf547d67159b3
leocastroz <castroz.dev@gmail.com>
2024-10-16 01:31:45 -0300

    fix: force HTTPS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Content Security Policy para atualizar requisições inseguras para HTTPS -->
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
